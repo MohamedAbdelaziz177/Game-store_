bf7d1c742e928ab4260e8ccc6edd8ecbab2a6731
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-05-20 23:42:02 +0300

    Adding more functionalities to repositories

--- src/main/java/com/_Abdelaziz26/WWE/Repositories/ParticipantsRepository.java
+++ src/main/java/com/_Abdelaziz26/WWE/Repositories/ParticipantsRepository.java
@@ -1,16 +1,13 @@
 package com._Abdelaziz26.WWE.Repositories;
 
-import com._Abdelaziz26.WWE.Model.MatchParticipants;
+import com._Abdelaziz26.WWE.Model.MatchParticipant;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
+public interface ParticipantsRepository extends JpaRepository<MatchParticipant, Long> {
 
-public interface ParticipantsRepository extends JpaRepository<MatchParticipants, Long> {
+    Page<MatchParticipant> findParticipantsByWrestler_Id(Long wrestlerId, Pageable pageable);
 
-    Page<MatchParticipants> findParticipantsByWrestler_Id(Long wrestlerId, Pageable pageable);
-
-    Page<MatchParticipants> findMatchParticipantsByMatch_Id(Long matchId, Pageable pageable);
+    Page<MatchParticipant> findMatchParticipantsByMatch_Id(Long matchId, Pageable pageable);
 }
