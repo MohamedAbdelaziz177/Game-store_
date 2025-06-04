ba5afc298eabef695a62970dbdcb6cf3062b717d
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-06-04 23:21:22 +0300

    Serializing DTOs to for caching

--- src/main/java/com/_Abdelaziz26/Game/DTOs/Game/GameCardDto.java
+++ src/main/java/com/_Abdelaziz26/Game/DTOs/Game/GameCardDto.java
@@ -5,15 +5,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class GameCardDto {
+public class GameCardDto implements Serializable {
 
     private Long id;
 
     private String name;
 
     private String imageUrl;
+
+    private Double price;
 }
