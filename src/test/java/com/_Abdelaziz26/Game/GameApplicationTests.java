1aff78984e934a09351289ec1ce2c602fa9344c0
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-09-21 09:00:23 +0300

    Adding the env variables to the CI/CD pipeline

--- src/test/java/com/_Abdelaziz26/Game/GameApplicationTests.java
+++ src/test/java/com/_Abdelaziz26/Game/GameApplicationTests.java
@@ -2,8 +2,10 @@ package com._Abdelaziz26.Game;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GameApplicationTests {
 
 	@Test
